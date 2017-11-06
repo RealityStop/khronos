d9e4ba872db5b455404d0b547e51264c644c9684
RealityStop <qubiquitycodes@gmail.com>
2017-11-06 13:31:42 -0500

    Fix humper linkage and add a (hardcoded) floor to the default level.

--- src/Duality/Source/Code/CorePlugin/World/Level.cs
+++ src/Duality/Source/Code/CorePlugin/World/Level.cs
@@ -1,4 +1,7 @@
-﻿namespace Khronos.World
+﻿using Humper;
+using System;
+
+namespace Khronos.World
 {
     internal class Level
     {
@@ -10,9 +13,19 @@
                 if (_Default == null)
                 {
                     _Default = new Level();
+                    _Default.AddFloor();
                 }
                 return _Default;
             }
+        }
+
+        IWorld world = new Humper.World(800, 600);
+
+
+
+        private void AddFloor()
+        {
+            world.Create(0, 750, 800, 50);
         }
     }
 }
