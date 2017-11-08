9426879bbcd139ef69bb9b0c1157e8f9bba3e7e3
RealityStop <qubiquityCodes@gmail.com>
2017-11-08 00:44:58 -0500

    Fixed optimizations.  Works great!

--- src/Duality/Source/Code/CorePlugin/World/TileMerger.cs
+++ src/Duality/Source/Code/CorePlugin/World/TileMerger.cs
@@ -28,69 +28,71 @@ namespace Khronos.World
                     }
                     else if (start_y >= 0)
                     {
-                        //If we found the end of the collider, then it's time to start merging.
-                        List<int> overlaps = new List<int>();
-                        for (int i = 0; i < rectangles.Count; i++)
-                        {
-                            Rect rect = rectangles[i];
-                            if (rect.LeftX == x - 1 &&
-                                start_y <= rect.TopY &&
-                                end_y >= rect.BottomY)
-                            {
-                                overlaps.Add(i);
-                            }
-                        }
-
-                        overlaps.Sort(Comparer<int>.Create((a, b) => { return rectangles[a].TopY.CompareTo(rectangles[b].TopY); }));
-
-
-                        for (int i = 0; i < overlaps.Count; i++)
-                        {
-                            Rect rect = rectangles[overlaps[i]];
-                            if (start_y < rect.TopY)
-                            {
-                                Rect newRect = new Rect(x, start_y, 0, rect.TopY - 1 - start_y);
-                                rectangles.Add(newRect);
-                                start_y = (int)rect.TopY;
-                            }
-
-                            if (start_y == rect.TopY)
-                            {
-                                rect.W += 1;
-                                rectangles[overlaps[i]] = rect;
-                                if (end_y == rect.BottomY)
-                                {
-                                    start_y = -1;
-                                    end_y = -1;
-                                }
-                                else if (end_y > rect.BottomY)
-                                {
-                                    start_y = (int)rect.BottomY + 1;
-                                }
-                            }
-                        }
-
-
-                        if (start_y >= 0)
-                        {
-                            Rect newRect = new Rect(x, start_y, 0, end_y - start_y);
-                            rectangles.Add(newRect);
-                            start_y = -1;
-                            end_y = -1;
-                        }
+                        CheckOverlaps(ref start_y, ref end_y, rectangles, x);
                     }
                 }
 
                 if (start_y >= 0)
                 {
-                    Rect newRect = new Rect(x, start_y, 0, end_y - start_y);
-                    rectangles.Add(newRect);
-                    start_y = -1;
-                    end_y = -1;
+                    CheckOverlaps(ref start_y, ref end_y, rectangles, x);
                 }
             }
 
             return rectangles;
+        }
+
+        private static void CheckOverlaps(ref int start_y, ref int end_y, List<Rect> rectangles, int x)
+        {
+            //If we found the end of the collider, then it's time to start merging.
+            List<int> overlaps = new List<int>();
+            for (int i = 0; i < rectangles.Count; i++)
+            {
+                Rect rect = rectangles[i];
+                if (rect.RightX == x - 1 &&
+                    start_y <= rect.TopY &&
+                    end_y >= rect.BottomY)
+                {
+                    overlaps.Add(i);
+                }
+            }
+
+            overlaps.Sort(Comparer<int>.Create((a, b) => { return rectangles[a].TopY.CompareTo(rectangles[b].TopY); }));
+
+
+            for (int i = 0; i < overlaps.Count; i++)
+            {
+                Rect rect = rectangles[overlaps[i]];
+                if (start_y < rect.TopY)
+                {
+                    Rect newRect = new Rect(x, start_y, 0, rect.TopY - 1 - start_y);
+                    rectangles.Add(newRect);
+                    start_y = (int)rect.TopY;
+                }
+
+                if (start_y == rect.TopY)
+                {
+                    rect.W += 1;
+                    rectangles[overlaps[i]] = rect;
+                    if (end_y == rect.BottomY)
+                    {
+                        start_y = -1;
+                        end_y = -1;
+                    }
+                    else if (end_y > rect.BottomY)
+                    {
+                        start_y = (int)rect.BottomY + 1;
+                    }
+                }
+            }
+
+
+            if (start_y >= 0)
+            {
+                Rect newRect = new Rect(x, start_y, 0, end_y - start_y);
+                rectangles.Add(newRect);
+                start_y = -1;
+                end_y = -1;
+            }
         }
 
         public static bool IsCollider(Tilemap tilemap, int x, int y)
